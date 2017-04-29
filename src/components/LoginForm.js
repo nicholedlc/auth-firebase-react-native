a46caec51822d42d9caf16ec78c61c1dc1f8a36c
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-04-28 19:38:40 -0700

    Add user login function

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common'
 
 export default class LoginForm extends Component {
-  state = { email: '' , password: '' };
+  state = { email: '' , password: '', error: '' };
+
+  onButtonPress = () => {
+    const { email, password } = this.state;
+    this.setState({ error: '' });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch((err) => {
+            this.setState({ error: err.message });
+          });
+      });
+  }
 
   render () {
     return (
@@ -25,12 +39,25 @@ export default class LoginForm extends Component {
             value={this.state.password}
           />
         </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress}>
             Log in
           </Button>
         </CardSection>
       </Card>
     );
   };
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
 }
